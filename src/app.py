0e841e9b8c077504ff7228b3bbb40f9069abc4b1
iwanttobelievee <artemkursaevkz@gmail.com>
2025-12-09 12:57:24 +0500

    feat: add placeholder for camera

--- src/app.py
+++ src/app.py
@@ -12,35 +12,33 @@ class EnrollmentGUI:
         self.lock = lock
         self.shared_frames = shared_frames
 
+        self.placeholder = ft.Container(
+            width=640,
+            height=480,
+            bgcolor=ft.Colors.GREY_100,
+            content=ft.Row(
+                controls=[ft.Icon(ft.Icons.CAMERA_ALT, size=100, color=ft.Colors.GREY_600)],
+                alignment=ft.MainAxisAlignment.CENTER,
+                vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            ),
+        )
+
+        self.image = ft.Image(
+            src_base64="",
+            width=640,
+            height=480,
+            fit=ft.ImageFit.CONTAIN,
+        )
+
+        self.frame = ft.Stack(
+            controls=[self.placeholder, self.image],
+            width=640,
+            height=480,
+        )
+
     def app(self, page: ft.Page):
         page.title = "Enrollment GUI"
         page.on_close = lambda e: self.stop_event.set()
-
-        if self.shared_frames["default"] is not None:
-            self.frame = ft.Image(
-                src_base64=self.shared_frames["default"],
-                width=640,
-                height=480,
-                fit=ft.ImageFit.CONTAIN,
-            )
-        else:
-            self.frame = ft.Image(
-                src="resources/img/placeholder.jpg",
-                width=640,
-                height=480,
-                fit=ft.ImageFit.CONTAIN,
-            )
-
-        @timer(self.fps, self.stop_event)
-        def update_frame():
-            with self.lock:
-                default_frame = self.shared_frames["default"]
-
-            if default_frame is not None:
-                self.frame.src_base64 = frame_to_base64(default_frame)
-                self.frame.update()
-
-        update_frame()
 
         page.add(
             ft.Row(
@@ -50,3 +48,22 @@ class EnrollmentGUI:
                 alignment=ft.MainAxisAlignment.CENTER,
             )
         )
+
+        @timer(self.fps, self.stop_event)
+        def update_frame():
+            with self.lock:
+                default_frame = self.shared_frames["default"]
+
+            has_frame = default_frame is not None
+
+            # Toggle visibility
+            self.image.visible = has_frame
+            self.placeholder.visible = not has_frame
+
+            if has_frame:
+                self.image.src_base64 = frame_to_base64(default_frame)
+
+            self.image.update()
+            self.placeholder.update()
+
+        update_frame()
